155d859a64549076681bcf1002d88558bcdf2007
aysenurkadim <72191246+aysenurkadim@users.noreply.github.com>
2024-10-12 17:12:29 +0300

    Update kodluyoruz öklid problemi.py

--- kodluyoruz öklid problemi.py
+++ kodluyoruz öklid problemi.py
@@ -1,26 +1,26 @@
 import math
 
-# 1. Noktaların Tanımlanması
+# 1. Noktaların Tanımlanma
 points = [(1, 2), (4, 6), (3, 1), (5, 5)]
 
-# 2. Öklid Mesafesi İçin Bir Fonksiyon Yazma
+# 2. Öklid Mesafesi hesaplama
 def euclideanDistance(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return distance
 
-# 3. Mesafelerin Hesaplanması
+# 3. Listedeki Mesafelerin Hesaplanması
 distances = []
 for i in range(len(points)):
     for j in range(i + 1, len(points)):  # Her nokta çiftini al
         distance = euclideanDistance(points[i], points[j])
         distances.append(distance)
 
-# 4. Minimum Mesafenin Bulunması
+# 4. Minimum Mesafeyi bulma
 min_distance = min(distances)
 
-# Sonucu yazdırma
+# Sonuc
 print(f"Noktalar: {points}")
-print(f"Hesaplanan mesafeler: {distances}")
+print(f"Mesafeler: {distances}")
 print(f"Minimum mesafe: {min_distance}")
